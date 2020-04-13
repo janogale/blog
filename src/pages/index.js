82be03a73b1cf88032cd31555b273cc1e09c521e
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-04-13 19:25:24 +0300

    Modify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
 
 class IndexPage extends React.Component {
   render() {
@@ -22,7 +23,27 @@ class IndexPage extends React.Component {
           </span>
         </h1>
         <p>Welcome to my personal Blog.</p>
-        <p>This is simple Blog developed for Learning and Teaching Web</p>
+        <p>
+          This is simple Blog developed for Learning and Teaching Web
+          Development
+        </p>
+        <h4>Specially I focus on</h4>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            marginBottom: "3rem",
+          }}
+        >
+          <Button>HTML</Button>
+          <Button>CSS</Button>
+          <Button>JavaScript</Button>
+          <Button>React JS</Button>
+          <Button>Gatsby</Button>
+          <Button>PWA</Button>
+          <Button>Web Components</Button>
+          <Button>UI & UX Design</Button>
+        </div>
         <p>
           To read posts click{" "}
           <span role="img" aria-label="hand poining up">
@@ -52,5 +73,19 @@ class IndexPage extends React.Component {
     )
   }
 }
+
+const Button = styled.button`
+  border: none;
+  border-radius: 3px;
+  margin: 12px 5px;
+  color: teal;
+  cursor: pointer;
+  padding: 2px 6px;
+  &:hover {
+    color: green;
+    font-weight: bolder;
+    background: #ccc;
+  }
+`
 
 export default IndexPage
